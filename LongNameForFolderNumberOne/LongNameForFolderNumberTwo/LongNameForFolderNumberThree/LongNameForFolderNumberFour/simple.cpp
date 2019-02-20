739a8d42ea41bfb3b0945e99bccd8c8e7ae4dfb8
Johan Laine (jolae) <johan.laine@tobii.com>
2019-02-20 14:10:25 +0100

    Initial commit

---
+++ LongNameForFolderNumberOne/LongNameForFolderNumberTwo/LongNameForFolderNumberThree/LongNameForFolderNumberFour/simple.cpp
@@ -0,0 +1,27 @@
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void foo() {
+    string line;
+    ifstream myfile ("./LongNameForFolderNumberOne/LongNameForFolderNumberTwo/LongNameForFolderNumberThree/LongNameForFolderNumberFour/example.txt");
+    cout << "Hello there" << endl;
+    ASSERT_TRUE(myfile.is_open());
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+}
+
+
+TEST(SimpleTest, ReadingTest)
+{
+    foo();
+}
